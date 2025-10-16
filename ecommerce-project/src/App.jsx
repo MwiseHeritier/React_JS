f5814b1377b2898ad403b59e36fc405f554344ab
MwiseHeritier <mwisehertier>
2025-10-16 21:33:01 +0200

    chore: moving pages related to checkout into a single folder

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route} from 'react-router';
 import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/checkoutPage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/ordersPage';
 import './App.css';
 
