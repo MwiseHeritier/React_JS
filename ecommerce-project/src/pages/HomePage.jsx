e5ddf75774000531c1632fcc84f835334ce2fe85
MwiseHeritier <mwisehertier>
2025-10-20 15:50:17 +0200

    chore: Add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -11,13 +11,13 @@ export function HomePage() {
 	const[cart, setCart] = useState([])
 
 	useEffect(() =>{
-		axios.get('http://localhost:3000/api/products')
+		axios.get('/api/products')
 			.then((response) =>{
 			setProducts(response.data);
 			
 			});
 
-		axios.get('http://localhost:3000/api/cart-items')
+		axios.get('/api/cart-items')
 			.then((response) =>{
 				setCart(response.data);
 			})
@@ -204,5 +204,20 @@ This only happens in development — it won’t happen in the production build.
 ->saves the fetched products into your state so you can display them.
 -> setProducts(response.data); this will saves the fetched/products data into products and use 'products' variable
 to generate HTML
+
+Server-proxy configuration
+-------------------------
+server: {
+	proxy: {
+		'/api' : {
+			target: 'http://localhost:3000'
+		},
+		'/images' : {
+			target: 'http://localhost:3000'
+		}
+	}
+}
+
+This means URL path starts with '/api ' the request will automaticall go to target'
 */
 }
